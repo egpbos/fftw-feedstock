b15fc6d98c32dca3576794c325f74d921c473804
Patrick Bos <egpbos@users.noreply.github.com>
2017-08-03 11:25:30 +0200

    Add long double build on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -46,3 +46,21 @@ if errorlevel 1 exit 1
 ctest --output-on-failure --timeout 100
 if errorlevel 1 exit 1
 
+del CMakeCache.txt
+
+cmake -LAH -G"NMake Makefiles"                             ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                   ^
+  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                      ^
+  -DENABLE_THREADS=ON                                      ^
+  -DWITH_COMBINED_THREADS=ON                               ^
+  -DENABLE_LONG_DOUBLE=ON                                  ^
+  ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure --timeout 100
+if errorlevel 1 exit 1
+
